df27412ab1d2a95cf24794326671c675dc862ea9
Magesh <tkmagesh77@gmail.com>
2019-06-11 11:08:11 +0530

    calculator-1 solution and calculator-2 assignment

---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './calculatorModel';
+
+@Component({
+	selector : 'app-calculator-1',
+	templateUrl : 'calculatorOne.component.html',
+	styleUrls : ['calculatorOne.component.css']
+})
+export class CalculatorOneComponent{
+	model : CalculatorModel = new CalculatorModel();
+}
+
+
+
+
+
+
+
+
+
+
+
