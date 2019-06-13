6404f3b72242a9fda94b45d6041d4595b2f50eb7
Magesh <tkmagesh77@gmail.com>
2019-06-13 10:04:14 +0530

    async demos - initial

---
+++ async-demos/async-demos.js
@@ -0,0 +1,33 @@
+(function(){
+	function addSync(x,y){
+		console.log(`	[@Service] processing ${x} and ${y}`);
+		var result = x + y;
+		console.log(`	[@Service] returning the result`);
+		return result;
+	}
+
+	function addSyncClient(x,y){
+		console.log(`[@Client] triggering the service`);
+		var result = addSync(x,y);
+		console.log(`[@Client] result = ${result}`);
+	}
+
+	window['addSyncClient'] = addSyncClient
+
+	function addAsync(x,y){
+		console.log(`	[@Service] processing ${x} and ${y}`);
+		setTimeout(function(){
+			var result = x + y;
+			console.log(`	[@Service] returning the result`);
+			return result;
+		},5000);
+	}
+
+	function addAsyncClient(x,y){
+		console.log(`[@Client] triggering the service`);
+		var result = addAsync(x,y);
+		console.log(`[@Client] result = ${result}`);
+	}
+
+	window['addAsyncClient'] = addAsyncClient
+})();
